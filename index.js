fc2fd2564bf2698856bffda3de8973efb54dc712
begie1 <jbergeline1@gmail.com>
2024-10-01 15:18:56 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import chalk from "chalk";
+
+console.log(
+    chalk.blue("Salvia!")
+);
+
+console.log(
+    chalk.red("Paul!")
+);
+
+console.log(
+    chalk.green("Julie!")
+);
+
+console.log(
+    chalk.yellow("Géraldine!")
+);
